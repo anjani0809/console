770cf8f5b6effb11267142523ed767eb08219e48
anjani0809 <91551538+anjani0809@users.noreply.github.com>
2022-01-18 18:50:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var school = "blossom"
+var school_second = "yagappa"
+
+var a = school.concat(school_second)
+console.log(school,school_second,a)
+
+var no_one = 8
+var no_two = 25
+var b = Math.sqrt(no_one)
+var c = Math.sqrt(no_two)
+var add = no_one + no_two
+var sub = no_two - no_one
+console.log(no_one,no_two,b,c,add,sub)
